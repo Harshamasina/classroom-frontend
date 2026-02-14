0fcdc3a961deef68854aae465f0439a167ae11e0
Harshamasina <harshamasina1999@gmail.com>
2026-02-14 15:50:57 -0600

    added: "Side Panel and Theme"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -6,44 +6,62 @@ import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
+import Dashboard from "./pages/dashboard";
+import { Book, Home } from "lucide-react";
+import { Layout } from "./components/refine-ui/layout/layout";
+import SubjectList from "./pages/subjects/list";
+import SubjectsCreate from "./pages/subjects/create";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ThemeProvider>
-          <DevtoolsProvider>
-            <Refine
-              dataProvider={dataProvider}
-              notificationProvider={useNotificationProvider()}
-              routerProvider={routerProvider}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                projectId: "BTGcNc-IcbPbg-xT2WLd",
-              }}
-            >
-              <Routes>
-                <Route index element={<WelcomePage />} />
-              </Routes>
-              <Toaster />
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
-            <DevtoolsPanel />
-          </DevtoolsProvider>
-        </ThemeProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+        <RefineKbarProvider>
+            <ThemeProvider>
+            <DevtoolsProvider>
+                <Refine
+                dataProvider={dataProvider}
+                notificationProvider={useNotificationProvider()}
+                routerProvider={routerProvider}
+                options={{
+                    syncWithLocation: true,
+                    warnWhenUnsavedChanges: true,
+                    projectId: "BTGcNc-IcbPbg-xT2WLd",
+                }}
+                resources={[
+                    {name: "dashboard", list: "/", meta: {label: 'Home', icon: <Home />}},
+                    {name: "subjects", list: "/subjects", create: "/subjects/create", meta: {label: 'Subjects', icon: <Book />}},
+                ]}
+                >
+                <Routes>
+                    <Route element={
+                        <Layout>
+                            <Outlet />  
+                        </Layout>
+                    }>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="subjects">
+                            <Route index element={<SubjectList />} />
+                            <Route path="create" element={<SubjectsCreate />} />
+                        </Route>
+                    </Route>
+                </Routes>
+                <Toaster />
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+                </Refine>
+                <DevtoolsPanel />
+            </DevtoolsProvider>
+            </ThemeProvider>
+        </RefineKbarProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
